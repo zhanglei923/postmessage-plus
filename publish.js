a980501574289be03b652b169e87458b693b99cc
zhanglei <zhanglei@xiaoshouyi.com>
2018-06-22 14:08:03 +0800

    up

--- publish.js
+++ publish.js
@@ -1,9 +1,11 @@
 let fs = require('fs');
 let pathUtil = require('path');
 
+let libPath = pathUtil.resolve(__dirname, './lib')
 let srcPath = pathUtil.resolve(__dirname, './src')
 let distPath = pathUtil.resolve(__dirname, './dist')
 
+let bluebird = fs.readFileSync(pathUtil.resolve(libPath, 'bluebird.min.js'),'utf8');
 let content = fs.readFileSync(pathUtil.resolve(srcPath, 'postmessage-plus.js'),'utf8');
 let content_cmd = `
 define(function(require, exports, module) {
@@ -16,6 +18,6 @@ export default postmessageplus;
 `
 console.log(content)
 
-fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.dist.js'), content); 
+fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.dist.js'), `${bluebird}\n${content}`); 
 fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.cmd.js'), content_cmd); 
 fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.module.js'), content_module); 
--- src/postmessage-plus.js
+++ src/postmessage-plus.js
@@ -1,15 +1,51 @@
+
+
+//postmessage-plus
 (function(){
-    var handleMessage = function () {
-        alert('23413')
-    }
     if (window.addEventListener) {                    //所有主流浏览器，除了 IE 8 及更早 IE版本
         window.addEventListener("message", handleMessage);
     } else if (window.attachEvent) {                  // IE 8 及更早 IE 版本
         window.attachEvent("onmessage", handleMessage);
     }
+    var handleMessage = function (data) {
+        if(data.__result_token){
+
+        }else{
+
+        }
+        
+    }
+    var handleResultMsg = function(){
+
+    }
+    var handleCall = function(){
+
+    }
+    //callback token, like: result-89876-1529647278723
+    var generateToken = function(){
+        var time = 1 * (new Date())
+        var random = Math.floor( Math.random() * Math.pow(10, 5));
+        return 'result-'+random+'-'+time;
+    }
+    var _currentTarget = window.parent;
     window.postmessageplus = {
+        setTarget: function(target){
+            _currentTarget = target;
+            return window.postmessageplus;
+        },
         call: function (){
+            var args = [];
+            for(var i = 0; i < arguments.length; i++){
+                args.push(arguments[i]);
+            }
+            console.log('args', args)
             parent.postMessage({}, '*');
+            _currentTarget = window.parent;//reverse back to parent by default!
+            var token = generateToken();
+            console.log(token)
+            return new Promise(function (resolve, reject) {  
+                resolve(111)
+            });
         }
     }
 })()
