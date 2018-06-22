711cb340ea89b496a8333445c43c5d6efbc4e211
zhanglei <zhanglei@xiaoshouyi.com>
2018-06-22 16:54:16 +0800

    up

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ let pathUtil = require('path');
 let libPath = pathUtil.resolve(__dirname, './lib')
 let srcPath = pathUtil.resolve(__dirname, './src')
 let distPath = pathUtil.resolve(__dirname, './dist')
+let npmPath = pathUtil.resolve(__dirname, '../postmessage-plus-npm')
 
 let bluebird = fs.readFileSync(pathUtil.resolve(libPath, 'bluebird.min.js'),'utf8');
 let content = fs.readFileSync(pathUtil.resolve(srcPath, 'postmessage-plus.js'),'utf8');
@@ -23,5 +24,5 @@ var clean = (content) =>{
 
 fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.dist.js'), `${bluebird}\n${content}`); 
 fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.cmd.js'), clean(content_cmd)); 
-fs.writeFileSync(pathUtil.resolve(distPath, 'postmessage-plus.module.js'), clean(content_module)); 
+fs.writeFileSync(pathUtil.resolve(npmPath, 'postmessage-plus.module.js'), clean(content_module)); 
 console.log('done.')
