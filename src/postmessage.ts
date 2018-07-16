839edc7692b13d630efb48aebbfcc1264fb2e87b
zhanglei <zhang.lei.923@gmail.com>
2018-07-17 05:48:22 +0800

    up

--- src/postmessage.ts
+++ src/postmessage.ts
@@ -1,1 +1,27 @@
+//postmessage-plus
+var postmessageplus;
+(() => {
+    var _currentTarget = window.parent;
+    var _currentTargetHost = '*';
+    var _waitingPromiseMap = {};
+    var _currentResult;
+
+    var handleMessage = function (data) {
+        //console.log('got msg', window.location.href, data)
+        var data = data.data;
+        if(data.__postmessageplus_token){
+            //handleCall(data)
+        }else if(data.__postmessageplus_result_token){
+            //handleResultMsg(data)
+        }        
+    }
+
+
+    if ((<any>window).addEventListener) {
+        (<any>window).addEventListener('message', ()=>{}, false)
+    } else if ((<any>window).attachEvent) {
+        (<any>window).attachEvent("onmessage", ()=>{});
+    }
+    
+})();
 (<any>window).postmessageplus={}
